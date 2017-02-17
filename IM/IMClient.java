f17708f1f5fcd41ba8e65b046382f736838810e4
Anna M. Pfoertsch <apfoerts@wellesley.edu>
2017-02-17 12:39:24 -0500

    Added comments to client interface

--- IM/IMClient.java
+++ IM/IMClient.java
@@ -8,6 +8,16 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IMClient extends Remote {
+    /* 
+    * getUserName()
+    * Returns the username of the IMClient. The username is used to index the           
+    * stubs on the server and display the name. 
+    */
    public String getUserName() throws RemoteException;
+    /* 
+    * receiveMessage(String messageText)
+    * Prints the username of the sender and the message content on the receiving 
+    * client’s console.
+    */
    public void receiveMessage(String messageText) throws RemoteException;
 }
